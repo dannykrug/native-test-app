6afc86d0afa6b829a610f77532cb6d59d2a93c98
dannykrug <dannykrug@gmail.com>
2019-01-03 11:47:11 -0500

    added components

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/** @format */
+//Index.ios.js - place code in here for iOS!!
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
 
-AppRegistry.registerComponent(appName, () => App);
+//Create a component
+const App = () => {
+  return (
+    <Header />
+  );
+};
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
