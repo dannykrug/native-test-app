10860de6fed8b6cb2cd7b449de415c4f440695b2
dannykrug <dannykrug@gmail.com>
2019-01-04 11:20:43 -0500

    more styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,31 @@
 // Import libraries for making a Component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 //Make a Component
-const Header = () => {
-  const { textStyle } = styles;
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
 
-    return <Text style={textStyle}>Albums!</Text>;
+    return (
+      <View style={viewStyle}>
+        <Text style={textStyle}>{props.headerText}</Text>
+      </View>
+    );
 };
 
 const styles = {
+    viewStyle: {
+      backgroundColor: '#f8f8f8',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 70,
+      paddingTop: 30,
+      shadowColor: 'black',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      elevation: 2,
+      position: 'relative'
+    },
     textStyle: {
       fontSize: 20
     }
