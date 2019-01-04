0227e11e59914cf47637025759cac4a897ec318f
dannykrug <dannykrug@gmail.com>
2019-01-04 10:49:35 -0500

    title styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 //Make a Component
-const Header = () => (
-    <Text>Albums!</Text>
-  );
+const Header = () => {
+  const { textStyle } = styles;
+
+    return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+    textStyle: {
+      fontSize: 20
+    }
+  };
 
 //Make the component available to other parts of the app
 export default Header;
