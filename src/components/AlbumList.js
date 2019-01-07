0b2720c112a559c708274efecf7bb57499fd932d
dannykrug <dannykrug@gmail.com>
2019-01-07 10:32:14 -0500

    fetching data

---
+++ src/components/AlbumList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Axios from 'axios';
+
+class AlbumList extends Component {
+
+  componentWillMount() {
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
+  }
+
+  render() {
+    return (
+      <View>
+      <Text>Album List</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
